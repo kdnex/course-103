c31b9a4ce2a9f515fdf846630e4260bcf3b0295e
ToddBooth <Git@ToddBooth.Com>
2023-09-26 00:28:03 +0200

    STUDNET_ASSIGNED_NUMBER

--- ansible/update_linux_server.sh
+++ ansible/update_linux_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 repo="ansible"
 cd $repo
-time ansible-playbook ansible/update_linux_server.yml
+# time ansible-playbook ansible/update_linux_server.yml
+pwd
+ls -l
+time ansible-playbook update_linux_server.yml
 
